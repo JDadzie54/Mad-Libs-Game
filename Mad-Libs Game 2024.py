e647b9e69268def4d2c30dc021745916944a7fcd
Jason Dadzie <jdadzie54@gmail.com>
2024-08-12 10:30:09 -0400

    this is my first commit lets goooooooooooooooooo

---
+++ Mad-Libs Game 2024.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plural_noun = input("Please Enter a plural noun: ")
+name = input("Please input a name ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + name)
